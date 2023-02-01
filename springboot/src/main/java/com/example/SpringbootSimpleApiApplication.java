3d120f19fd8ee77194818dbcd4b8d30a48bf74f2
tomas6446 <tomas.kozakas@gmail.com>
2023-02-01 15:25:34 +0200

    Create JwtAuthFilter.java

---
+++ springboot/src/main/java/com/example/SpringbootSimpleApiApplication.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Tomas Kozakas
+ */
+@SpringBootApplication
+public class SpringbootSimpleApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSimpleApiApplication.class, args);
+    }
+}
---
+++ springboot/src/main/java/com/example/config/JwtAuthFilter.java
@@ -0,0 +1,55 @@
+package com.example.config;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
+/**
+ * @author Tomas Kozakas
+ */
+public class JwtAuthFilter extends OncePerRequestFilter {
+    private final UserDetailsService userDetailsService;
+    private final JwtUtils jwtUtils;
+
+    public JwtAuthFilter(UserDetailsService userDetailsService, JwtUtils jwtUtils) {
+        this.userDetailsService = userDetailsService;
+        this.jwtUtils = jwtUtils;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest,
+                                    HttpServletResponse httpServletResponse,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        final String authHeader = httpServletRequest.getHeader(AUTHORIZATION);
+        final String userEmail;
+        final String jwtToken;
+        if (authHeader == null || !authHeader.startsWith("Bearer")) {
+            filterChain.doFilter(httpServletRequest, httpServletResponse);
+            return;
+        }
+        jwtToken = authHeader.substring(7);
+        userEmail = jwtUtils.extractUsername(jwtToken);
+        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = userDetailsService.loadUserByUsername(userEmail);
+            final boolean isTokenValid;
+            if (jwtUtils.validateToken(jwtToken, userDetails)) {
+                UsernamePasswordAuthenticationToken authenticationToken =
+                        new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(httpServletRequest));
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            }
+        }
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
+    }
+}
